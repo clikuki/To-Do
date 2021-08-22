7e46cc9e57a753e58230301ac7b975f63f62420a
Clikuki <73450123+clikuki@users.noreply.github.com>
2021-08-22 22:00:07 +0800

    changes to project module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import projects from './modules/projects';
 import './normalize.css';
 import './style.css';
 
-const testTodoInfo = {
+const testInfo1 = {
 	title: 'test title',
 	description: 'A descriptive description',
 	dueDate: '08/07/21',
 	priority: 1,
 };
+
+window.projects = projects;
---
+++ src/modules/projects.js
@@ -0,0 +1,212 @@
+const getIndexFromKey = (array, key) =>
+{
+	if (typeof key !== 'string') throw new Error('Invalid key: must be a string');
+	const index = array.findIndex(item => item.key === key);
+	if (index === -1) throw new Error('Invalid key: item not found');
+
+	return index;
+}
+
+const getUniqueKey = (() =>
+{
+	const getRandomInt = (min, max) =>
+	{
+		min = Math.ceil(min);
+		max = Math.floor(max);
+
+		const randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+
+		return randomInt;
+	}
+
+	const getRandChar = () => chars.charAt(Math.floor(Math.random() * charsLen))
+
+	const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	const charsLen = chars.length;
+	const keylen = {
+		min: 10,
+		max: 20,
+	};
+
+	return () =>
+	{
+		const length = getRandomInt(keylen.min, keylen.max);
+		const result = Array.from({ length }, () => getRandChar()).join('');
+
+		return result;
+	}
+})()
+
+const removeAtIndex = (array, index) => array.splice(index, index + 1);
+
+const todoMethods = (() =>
+{
+	const get =  (() =>
+	{
+		const deepCopy = array =>
+		{
+			const newArr = [];
+
+			for (const obj of array)
+			{
+				const newObj = {};
+				for (const [key, val] of Object.entries(obj))
+				{
+					newObj[key] = val;
+				}
+
+				newArr.push(newObj);
+			}
+
+			return newArr;
+		}
+
+		return todoList => deepCopy(todoList);
+	})()
+
+	const add = (() =>
+	{
+		const keyCheck = (() =>
+		{
+			const reqKeys = [
+				'title', 'description', 'dueDate', 'priority',
+			]
+
+			return todoInfo =>
+			{
+				let numOfKeys = 0;
+	
+				for (const key in todoInfo)
+				{
+					if(reqKeys.includes(key)) ++numOfKeys;
+					else return false;
+				}
+	
+				if(numOfKeys !== reqKeys.length) return false;
+	
+				return true;
+			}
+		})()
+
+		return (todoList, todoInfo) =>
+		{
+			if (typeof todoInfo !== 'object')
+			{
+				throw new Error('Invalid itemInfo: must be an object');
+			}
+
+			if (!keyCheck(todoInfo))
+			{
+				throw new Error('Invalid itemInfo: invalid or missing key/s');
+			}
+
+			todoList.push({
+				key: getUniqueKey(),
+				completed: false,
+				...todoInfo,
+			});
+		}
+	})()
+
+	const remove = (todoList, key) =>
+	{
+		const todoIndex = getIndexFromKey(todoList, key);
+		return removeAtIndex(todoList, todoIndex);
+	}
+
+	const edit = (() =>
+	{
+		const editProps = (obj, editObj) =>
+		{
+			for (const [key, val] of Object.entries(editObj))
+			{
+				if ({}.hasOwnProperty.call(obj, key))
+				{
+					obj[key] = val;
+				}
+			}
+		}
+
+		return (todoList, key, editObj) =>
+		{
+			const todoIndex = getIndexFromKey(todoList, key);
+			editProps(todoList[todoIndex], editObj);
+		}
+	})()
+
+	return {
+		get,
+		add,
+		remove,
+		edit,
+	}
+})()
+
+const projectMethods = (() =>
+{
+	const projectArray = [];
+
+	const get = (() =>
+	{
+		const dupe = (obj) => Object.fromEntries(Object.entries(obj));
+
+		return (key) =>
+		{
+			if(typeof key !== 'string')
+			{
+				return projectArray.map(project => dupe(project));
+			}
+			else
+			{
+				const projectIndex = getIndexFromKey(projectArray, key)
+				return projectArray[projectIndex];
+			}
+		}
+	})();
+
+	const add = (projectName) =>
+	{
+		if (typeof projectName !== 'string') return;
+
+		const todoList = [];
+		const project = {
+			projectName,
+			key: getUniqueKey(),
+			todos: {
+				get: () => todoMethods.get(todoList),
+				add: todoInfo => todoMethods.add(todoList, todoInfo),
+				remove: key => todoMethods.remove(todoList, key),
+				edit: (key, editObj) => todoMethods.edit(todoList, key, editObj),
+			},
+		}
+
+		projectArray.push(project);
+		return project;
+	}
+
+	const remove = (key) =>
+	{
+		const projectIndex = getIndexFromKey(projectArray, key);
+		return removeAtIndex(projectArray, projectIndex);
+	}
+
+	const changeName = (() =>
+	{
+		return (key, newName) =>
+		{
+			if(typeof newName !== 'string') return;
+
+			const projectIndex = getIndexFromKey(projectArray, key);
+			projectArray[projectIndex].projectName = newName;
+		};
+	})()
+
+	return {
+		get,
+		add,
+		remove,
+		changeName,
+	}
+})()
+
+export default projectMethods;
